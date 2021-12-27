ecb399d0b473c32727320d66d2c951f573c606ee
guita13921 <guita13921@gmail.com>
2021-12-27 19:47:14 +0700

    lab14_1.cpp  complete

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,39 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double x[],int y,double z[]){
+    double sum = 0;
+    for(int i=0 ; i < y; i++ ){
+        sum += x[i];
+    }
+    z[0] = sum/y;
+    for(int i = 0; i < y; i++ ){
+        double stan;
+        stan += pow(x[i] - z[0],2);
+        z[1] = sqrt(stan / y);
+    }
+    double Geo = 1;
+    for (int i = 0; i < y; i++){
+        Geo *= x[i];
+        z[2] = pow(Geo, (double)1/y);
+    }
+    double Harmo;
+    for(int i = 0; i < y; i++){
+        Harmo += (double)1 / x[i];
+        z[3] = (double)y/Harmo;
+    }
+    double max , min;
+    int i = 0;
+    max = x[i];
+    min = x[i];
+        for(int i=1; i < y; i++){
+            if(x[i] > max){
+                max = x[i];
+            }if(x[i] < min){
+                min = x[i];
+            }
+        }
+    z[4] = max;
+    z[5] = min;
+}
